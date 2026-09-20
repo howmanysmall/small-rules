83e6923697a04c2a0ef29d78486da00d7d451450
howmanysmall <26746527+howmanysmall@users.noreply.github.com>
2026-09-20 13:33:33 -0600

    chore(commitlint): use RuleConfigSeverity enum in commitlint config
    Signed-off-by: howmanysmall <26746527+howmanysmall@users.noreply.github.com>

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,15 +1,17 @@
+import { RuleConfigSeverity } from "@commitlint/types";
+
 import type { UserConfig } from "@commitlint/types";
 
 const configuration: UserConfig = {
 	extends: ["@commitlint/config-conventional"],
 	rules: {
-		"header-max-length": [2, "always", 72],
-		"scope-case": [2, "always", "lower-case"],
-		"subject-empty": [2, "never"],
-		"subject-full-stop": [2, "never", "."],
-		"type-case": [2, "always", "lower-case"],
+		"header-max-length": [RuleConfigSeverity.Error, "always", 72],
+		"scope-case": [RuleConfigSeverity.Error, "always", "lower-case"],
+		"subject-empty": [RuleConfigSeverity.Error, "never"],
+		"subject-full-stop": [RuleConfigSeverity.Error, "never", "."],
+		"type-case": [RuleConfigSeverity.Error, "always", "lower-case"],
 		"type-enum": [
-			2,
+			RuleConfigSeverity.Error,
 			"always",
 			["feat", "fix", "refactor", "docs", "style", "test", "chore", "perf", "ci", "build"],
 		],
